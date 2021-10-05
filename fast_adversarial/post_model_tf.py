b245226db1aa1251d2b8b73393c6c44b03f416c2
JokerYan <770792058@qq.com>
2021-10-05 21:30:22 +0800

    create default image_size, num_channels, num_labels for PostModelTf

--- fast_adversarial/post_model_tf.py
+++ fast_adversarial/post_model_tf.py
@@ -4,6 +4,9 @@ from .post_model import PostModel
 class PostModelTf():
     def __init__(self):
         self.post_model = PostModel()
+        self.image_size = 32
+        self.num_channels = 3
+        self.num_labels = 10
 
     def predict(self, x):
         print("===========================================")
