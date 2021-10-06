b62557378acb6da47326c0ec518c2ee4157a797a
JokerYan <770792058@qq.com>
2021-10-07 00:35:59 +0800

    limit cuda_visible_devices

--- test_all.sh
+++ test_all.sh
@@ -1,1 +1,1 @@
-python3 test_all.py -a black -d cifar10 -n 200 --solver adam -b 9 -s "black_results"
+CUDA_VISIBLE_DEVICES=0 python3 test_all.py -a black -d cifar10 -n 200 --solver adam -b 9 -s "black_results"
