f81ca0d7611e697f7d5e937da8a64c4b80ad5dbc
JokerYan <770792058@qq.com>
2021-10-07 17:07:07 +0800

    remove update post model

--- fast_adversarial/post_model.py
+++ fast_adversarial/post_model.py
@@ -48,18 +48,18 @@ class PostModel(nn.Module):
 
         self.post_model = None
 
-    def update_post_model(self, images):
-        sample_images = images[0, :, :, :].unsqueeze(0)
-        sample_images = self.transform(sample_images)
-        del self.post_model
-        post_model, original_class, neighbour_class, loss_list, acc_list, neighbour_delta = \
-            post_train(self.model, sample_images, self.train_loader, self.train_loaders_by_class, self.args)
-        self.post_model = post_model
+    # def update_post_model(self, images):
+    #     sample_images = images[0, :, :, :].unsqueeze(0)
+    #     sample_images = self.transform(sample_images)
+    #     del self.post_model
+    #     post_model, original_class, neighbour_class, loss_list, acc_list, neighbour_delta = \
+    #         post_train(self.model, sample_images, self.train_loader, self.train_loaders_by_class, self.args)
+    #     self.post_model = post_model
 
     def forward(self, images):
         images = self.transform(images)
-        # sample_images = images[0, :, :, :].unsqueeze(0)
-        # post_model, original_class, neighbour_class, loss_list, acc_list, neighbour_delta = \
-        #     post_train(self.model, sample_images, self.train_loader, self.train_loaders_by_class, self.args)
-        return self.post_model(images)
+        sample_images = images[0, :, :, :].unsqueeze(0)
+        post_model, original_class, neighbour_class, loss_list, acc_list, neighbour_delta = \
+            post_train(self.model, sample_images, self.train_loader, self.train_loaders_by_class, self.args)
+        return post_model(images)
         # return self.model(images)
